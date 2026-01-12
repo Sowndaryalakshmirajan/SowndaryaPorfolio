27e0db017cf25ef82d0f516813db5d9f958ab35c
unknown <ssowndaryalakshmirajan@gmail.com>
2026-01-13 00:57:15 +0530

    Added WhiteNoise to requirements.txt

--- myportfolio/myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/myportfolio/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!!oj2e7+4-hsme44yi1_%wi3+(q3w1ald7arqir6%k60b#9=a!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True    
 ALLOWED_HOSTS = ['*']
 
 
@@ -54,7 +53,7 @@ INSTALLED_APPS = [
 # ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -131,7 +130,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE='whitenoise.storage.CompressedMainfestStaticFilesStorage'
